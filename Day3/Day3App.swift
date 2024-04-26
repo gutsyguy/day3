000bb800c1607e880b192a877a557714e1d480ae
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2024-04-26 16:37:46 -0700

    Initial Commit

---
+++ Day3/Day3App.swift
@@ -0,0 +1,17 @@
+//
+//  Day3App.swift
+//  Day3
+//
+//  Created by admin on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Day3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
