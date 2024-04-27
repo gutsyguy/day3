b5d114d3bee80dfcd065b0cdf9048457538c11c4
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2024-04-26 17:08:10 -0700

    e

--- Day3/ContentView.swift
+++ Day3/ContentView.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        @State var animal = "Dog"
+        @State var zaint = "Dog"
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Picker("Select:", selection: $zaint){
+                Text("Angel").tag("Angel")
+                Text("Saint").tag("Saint")
+                Text("Devil").tag("Devil")
+            }.pickerStyle(.wheel)
         }
         .padding()
     }
